e0a8b16026bb805ca47930119162da0b97ee759f
Jessa Marie Barre <jmlbarre@gmail.com>
2017-09-12 16:40:26 -0400

    handled all topic selection events

--- src/App.js
+++ src/App.js
@@ -3,8 +3,52 @@ import './App.css'
 
 import TopicPickerContainer from './containers/TopicPickerContainer'
 
+/**
+ * App is the component that holds the entire application
+ */
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      selectedTopic: {}
+    }
+
+    this.setTopic = this.setTopic.bind(this)
+  }
+
+  /*
+    setTopic is called by TopicPickerContainer when a user
+    has submitted a topic
+   */
+  setTopic (topic) {
+    console.log('setting topic to', topic)
+    this.setState({
+      selectedTopic: topic
+    })
+  }
+
+  /* isEmpty checks if an object is empty */
+  isEmpty (obj) {
+    return Object.keys(obj).length === 0 && obj.constructor === Object
+  }
+
+  /*
+    Container returns the proper container based on the
+    value of selectedTopic
+  */
+  Container () {
+    if (!this.isEmpty(this.state.selectedTopic)) {
+      console.log('empty')
+    } else {
+      return (
+        <TopicPickerContainer setTopic={this.setTopic} />
+      )
+    }
+  }
+
   render () {
+    var container = this.Container()
+
     return (
       <div className='App'>
         <header>
@@ -14,7 +58,7 @@ class App extends Component {
         <main>
           <div className='container'>
             <div className='flashcard'>
-              <TopicPickerContainer />
+              {container}
             </div>
           </div>
         </main>
--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import './TopicList.css'
 
-const topics = ['Biology', 'Math', 'Music']
-
+/**
+ * TopicList is the component that displays a list of topics
+ */
 class TopicList extends Component {
-
-  /* getCorrespondingTopics fills the autocomplete dropdown
-    with topics that match the input data
-   */
+  /*
+    ListItems fills the dropdown with topics that match the
+    input data
+  */
   ListItems () {
     if (this.props.topics && this.props.topics.length > 0) {
-      return this.props.topics.map((topic) => {
+      return this.props.topics.map((topic, index) => {
         return (
-          <li key={topic.id}>{topic.name}</li>
+          <li key={index}
+            className={this.props.selectedIndex === index ? 'selected' : ''}
+            onClick={(e) => this.props.onTopicSelect(e, topic.id, index)}>{topic.name}</li>
         )
       })
     }
@@ -20,23 +23,19 @@ class TopicList extends Component {
     return null
   }
 
-  // use JS to listen for the input change event
-  // run a function whenever that fires
-  // delivering the current contents to your code as a variable.
-  // This function would use that variable to search your list and create a subset,
-  // then iterate through the subset and create a series of elements.
-  // You can use CSS to heavily style a ul so that it doesn't resemble a list at all,
-  //  and position it immediately under your input so that it appears to be a dropdown.
-  // The ul is useful because it can handle positioning its children.
-  //  These children can be anything you want. Buttons, anchors, other form elements, whatever.
   render () {
     var listItems = this.ListItems()
-
     var list = listItems ? (<ul> {listItems} </ul>) : null
 
     return (
-      <form className='TopicList'>
-        <input list='topics' onChange={this.props.onInputChange} placeholder='Enter a topic (e.g. Math)' />
+      <form className='TopicList' onSubmit={this.props.onTopicSubmit}>
+        <div className='input-wrapper'>
+          <input list='topics'
+            onChange={this.props.onInputChange}
+            onKeyDown={this.props.onListTraversal}
+            placeholder='Enter a topic (e.g. Math)' />
+          {/* <span className="autocomplete"></span> */}
+        </div>
         {list}
       </form>
     )
