c5f985c0067ccbdb5ad71b73ecc79ad5f9de105b
Jessamarie <jessamarie@users.noreply.github.com>
2023-08-31 12:56:19 -0400

    updated api url

--- src/containers/TopicPickerContainer.js
+++ src/containers/TopicPickerContainer.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import TopicList from '../components/TopicList'
+import React, { Component } from 'react';
+import TopicList from '../components/TopicList';
 
-import './TopicPickerContainer.css'
-import axios from 'axios'
+import './TopicPickerContainer.css';
+import axios from 'axios';
 
 /**
  * TopicList is the component that handles events related
  * to a user selecting a topic
  */
 class TopicPickerContainer extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       topics: [],
       matchingTopics: [],
@@ -18,141 +18,152 @@ class TopicPickerContainer extends Component {
       selectedID: 0, // no id can be 0
       /* stores the index of the currently selected topic in the dropdown */
       selectedIndex: -1
-    }
+    };
 
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleTopicSubmit = this.handleTopicSubmit.bind(this)
-    this.handleTopicSelect = this.handleTopicSelect.bind(this)
-    this.handleListTraversal = this.handleListTraversal.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleTopicSubmit = this.handleTopicSubmit.bind(this);
+    this.handleTopicSelect = this.handleTopicSelect.bind(this);
+    this.handleListTraversal = this.handleListTraversal.bind(this);
   }
 
   /* This will call the api to get all the topic data */
-  componentDidMount () {
-    axios.get('https://recall-app-api.herokuapp.com/api/topics').then((response) => {
-      this.setState({
-        topics: response.data
+  componentDidMount() {
+    axios
+      .get('https://recall-api.onrender.com/api/topics')
+      .then((response) => {
+        this.setState({
+          topics: response.data
+        });
       })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   /* findMatchingTopics returns a list of topic names that
      the user input startsWith
   */
-  findMatchingTopics (input) {
-    var topics = []
+  findMatchingTopics(input) {
+    var topics = [];
 
     if (input !== '') {
       topics = this.state.topics.filter((topic) => {
-        return topic.name.toLowerCase().startsWith(input.toLowerCase())
-      })
+        return topic.name.toLowerCase().startsWith(input.toLowerCase());
+      });
     }
 
-    return topics
+    return topics;
   }
 
-  handleInputChange (e) {
-    var input = e.target.value
-    var topics = this.findMatchingTopics(input)
+  handleInputChange(e) {
+    var input = e.target.value;
+    var topics = this.findMatchingTopics(input);
     this.setState({
       matchingTopics: topics,
       selectedID: 0,
       selectedIndex: -1
-    })
+    });
   }
 
   /* matchingTopicsExist returns true if the matchingTopics
     array is not empty, o.w false
   */
-  matchingTopicsExist () {
+  matchingTopicsExist() {
     if (this.state.matchingTopics && this.state.matchingTopics.length > 0) {
-      return true
+      return true;
     }
 
-    return false
+    return false;
   }
 
   /* handleTopicSubmit returns the selected Topic
     to the App, to be passed on to the Sentences
     component for preparation.
   */
-  handleTopicSubmit (e) {
-    e.preventDefault()
+  handleTopicSubmit(e) {
+    e.preventDefault();
 
     // do nothing if the index is less than or greater than
     // the length of the current matchingTopics
-    if (this.state.selectedIndex < 0 || this.state.selectedIndex > this.state.matchingTopics.length) { return }
+    if (
+      this.state.selectedIndex < 0 ||
+      this.state.selectedIndex > this.state.matchingTopics.length
+    ) {
+      return;
+    }
 
-    this.props.setTopic(this.state.matchingTopics[this.state.selectedIndex])
+    this.props.setTopic(this.state.matchingTopics[this.state.selectedIndex]);
     this.setState({
       selectedID: 0,
       selectedIndex: -1
-    })
+    });
   }
 
   /*
     handleTopicSelect changes the selectedID
     to the one that the user clicked
   */
-  handleTopicSelect (e, id, index) {
+  handleTopicSelect(e, id, index) {
     this.setState({
       selectedID: id,
       selectedIndex: index
-    })
+    });
   }
 
   /*
     moveSelectionUp sets the selectedIndex and id
     to the previous one's in matchingTopics
   */
-  moveSelectionUp () {
-    var newSelectedIndex = this.state.selectedIndex - 1
+  moveSelectionUp() {
+    var newSelectedIndex = this.state.selectedIndex - 1;
 
     // prevent index out of bounds
-    if (newSelectedIndex < 0) { return }
-    var newSelectedID = this.state.matchingTopics[newSelectedIndex].id
+    if (newSelectedIndex < 0) {
+      return;
+    }
+    var newSelectedID = this.state.matchingTopics[newSelectedIndex].id;
 
     this.setState({
       selectedIndex: newSelectedIndex,
       selectedID: newSelectedID
-    })
+    });
   }
 
   /*
     moveSelectionDown sets the selectedIndex and id
     to the next one's in matchingTopics
   */
-  moveSelectionDown () {
-    var newSelectedIndex = this.state.selectedIndex + 1
+  moveSelectionDown() {
+    var newSelectedIndex = this.state.selectedIndex + 1;
     // prevent index out of bounds
-    if (newSelectedIndex > this.state.matchingTopics.length - 1) { return }
-    var newSelectedID = this.state.matchingTopics[newSelectedIndex].id
+    if (newSelectedIndex > this.state.matchingTopics.length - 1) {
+      return;
+    }
+    var newSelectedID = this.state.matchingTopics[newSelectedIndex].id;
 
     this.setState({
       selectedIndex: newSelectedIndex,
       selectedID: newSelectedID
-    })
+    });
   }
 
   /*
     handleListTraversal handles the event where the
     user hits the down or up key
   */
-  handleListTraversal (e) {
+  handleListTraversal(e) {
     if (this.matchingTopicsExist()) {
       // if the keyCode is the 'UP' key
       if (e.keyCode === 38) {
-        this.moveSelectionUp()
-      // if the keyCode is the 'DOWN' key
+        this.moveSelectionUp();
+        // if the keyCode is the 'DOWN' key
       } else if (e.keyCode === 40) {
-        this.moveSelectionDown()
+        this.moveSelectionDown();
       }
     }
   } // end handleKeyDown
 
-  render () {
+  render() {
     return (
       <div className='TopicPickerContainer'>
         <TopicList
@@ -164,8 +175,8 @@ class TopicPickerContainer extends Component {
           onListTraversal={this.handleListTraversal}
         />
       </div>
-    )
+    );
   }
 }
 
-export default TopicPickerContainer
+export default TopicPickerContainer;
